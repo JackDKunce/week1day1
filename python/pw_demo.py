fff07750a1137da7cf68ba9a2700b8f1366c5f19
JackDKunce <jackdkunce@gmail.com>
2024-02-19 19:49:33 -0600

    first commit

---
+++ python/pw_demo.py
@@ -0,0 +1,15 @@
+def namer(f_name):
+    print("welcome to CT " + f_name + "!")
+    print("This will print too!")
+    return 
+
+namer("Jack")
+print(namer("jack"))
+
+def adder(num1, num2):
+    return num1 + num2
+
+
+print(adder(5, 6))
+
+
